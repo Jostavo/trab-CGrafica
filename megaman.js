5369b88dc65414ebb562b69797618e14d5a0dd93
Gustavo Rodrigues <gustavo._.r@hotmail.com>
2016-05-27 22:23:44 -0300

    pequenas modificações no código para tentar mostrar o bg e o fg

--- megaman.js
+++ megaman.js
@@ -17,10 +17,9 @@ renderer.render(scene, camera);
 
 function init() {
   var ScreenWidth, ScreenHeight;
-  var viewAngle = 0;
-  var aspect;
-  var near = 0.1;
-  var far = 20000;
+  var viewAngle = 40;
+  var near = 1;
+  var far = 100;
 
     backgroundTexture = loader.load( 'sprites/background/background.png' );
     foregroundTexture = loader.load( 'sprites/background/foreground.png' );
@@ -30,26 +29,24 @@ function init() {
     ScreenWidth = window.innerWidth;
     ScreenHeight = window.innerHeight;
 
-    aspect = ScreenWidth/ScreenHeight;
-
     camera = new THREE.PerspectiveCamera(viewAngle, aspect, near, far);
     scene.add(camera);
-    camera.position.set(0,0,10);
+    camera.position.set(10,0,0);
     camera.lookAt(scene.position);
 
 	  renderer = new THREE.WebGLRenderer( {antialias:true} );
     renderer.setSize(ScreenWidth, ScreenHeight);
 
-    // container = document.getElementById('ThreeJS');
-	  // container.appendChild( renderer.domElement );
+    container = document.getElementById('ThreeJS');
+	  container.appendChild( renderer.domElement );
 
     // controls = new THREE.MMXControls(camera, renderer.domElement);
 
     // var light = new THREE.PointLight(0xffffff);
 	  // light.position.set(100,250,100);
-	  // scene.add(light
+	  // scene.add(light)
 
-    backgroundMaterial = new THREE.MeshBasicMaterial( { map: backgroundTexture, side: THREE.FrontSide } );
+    backgroundMaterial = new THREE.MeshBasicMaterial( { map: backgroundTexture, side: THREE.DoubleSide } );
     foregroundMaterial = new THREE.MeshBasicMaterial( { map: foregroundTexture, side: THREE.DoubleSide } );
 
     backgroundPlane = new THREE.PlaneGeometry( 3545,352 );
@@ -57,6 +54,8 @@ function init() {
 
     background = new THREE.Mesh(backgroundPlane, backgroundMaterial);
     foreground = new THREE.Mesh(foregroundPlane, foregroundMaterial);
+    background.position.set(1,1,1);
+    foreground.position.set(1,1,1);
 
     scene.add(background);
     scene.add(foreground);
