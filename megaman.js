2a9c606b89594f3da2523767bf52239ba9ec906c
Gustavo Rodrigues <gustavo._.r@hotmail.com>
2016-05-29 20:39:57 -0300

    mudada a resolução

--- megaman.js
+++ megaman.js
@@ -110,33 +110,41 @@ function update()
   }
 }
 
-function TileTextureAnimator(texture, hTiles, vTiles, durationTile)
+function TextureAnimator(texture, tilesHoriz, tilesVert, numTiles, tileDispDuration)
 {
-  this.currentTile = 0;
-  this.durationTile = durationTile;
-  this.currentTime = 0;
-  this.hTiles = hTiles;
-  this.vTiles = vTiles;
-  this.cntTiles = this.hTiles * this.vTiles;
-  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+	// note: texture passed by reference, will be updated by the update function.
 
-  texture.repeat.set(1 / this.hTiles, 1 / this.vTiles);
+	this.tilesHorizontal = tilesHoriz;
+	this.tilesVertical = tilesVert;
+	// how many images does this spritesheet contain?
+	//  usually equals tilesHoriz * tilesVert, but not necessarily,
+	//  if there at blank tiles at the bottom of the spritesheet.
+	this.numberOfTiles = numTiles;
+	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+	texture.repeat.set( 1 / this.tilesHorizontal, 1 / this.tilesVertical );
 
-  this.update = function(time) {
-    this.currentTime += time;
+	// how long should each image be displayed?
+	this.tileDisplayDuration = tileDispDuration;
 
-    while (this.currentTime > this.durationTile) {
-      this.currentTime -= this.durationTile;
-      this.currentTile++;
+	// how long has the current image been displayed?
+	this.currentDisplayTime = 0;
 
-      if (this.currentTile == this.cntTiles) {
-        this.currentTile = 0;
-      }
+	// which image is currently being displayed?
+	this.currentTile = 0;
 
-      var iColumn = this.currentTile % this.hTiles;
-      texture.offset.x = iColumn / this.hTiles;
-      var iRow = Math.floor(this.currentTile / this.hTiles);
-      texture.offset.y = iRow / this.vTiles;
-    }
-  };
+	this.update = function( milliSec )
+	{
+		this.currentDisplayTime += milliSec;
+		while (this.currentDisplayTime > this.tileDisplayDuration)
+		{
+			this.currentDisplayTime -= this.tileDisplayDuration;
+			this.currentTile++;
+			if (this.currentTile == this.numberOfTiles)
+				this.currentTile = 0;
+			var currentColumn = this.currentTile % this.tilesHorizontal;
+			texture.offset.x = currentColumn / this.tilesHorizontal;
+			var currentRow = Math.floor( this.currentTile / this.tilesHorizontal );
+			texture.offset.y = currentRow / this.tilesVertical;
+		}
+	};
 }
