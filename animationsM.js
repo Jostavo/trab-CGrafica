01fce937a94380bb9f9167352dc7427263b2c43a
Gustavo Rodrigues <gustavo._.r@hotmail.com>
2016-05-30 21:59:50 -0300

    melhorando temporização da textura

--- animationsM.js
+++ animationsM.js
@@ -6,7 +6,7 @@ var standMegamanMaterial, walkMegamanMaterial, jumpMegamanMaterial, dashMegamanM
 var standMegamanAnim, walkMegamanAnim, jumpMegamanAnim, dashMegamanAnim;
 
 var animEsquerda = false;
-var standingClock = 30, runningClock = 450;
+var standingClock = 150, runningClock = 450;
 
 function initAnim(x, y, z)
 {
@@ -85,35 +85,31 @@ function changeSide()
 // Função de animação
 function TextureAnimator(texture, tilesHoriz, tilesVert, numTiles, tileDispDuration)
 {
-	// note: texture passed by reference, will be updated by the update function.
+	this.currentTile = 0;
+
+	this.tileDisplayDuration = tileDispDuration;
+
+	this.currentDisplayTime = 0;
 
 	this.tilesHorizontal = tilesHoriz;
 	this.tilesVertical = tilesVert;
-	// how many images does this spritesheet contain?
-	//  usually equals tilesHoriz * tilesVert, but not necessarily,
-	//  if there at blank tiles at the bottom of the spritesheet.
 	this.numberOfTiles = numTiles;
+
 	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
 	texture.repeat.set( 1 / this.tilesHorizontal, 1 / this.tilesVertical );
-
-	// how long should each image be displayed?
-	this.tileDisplayDuration = tileDispDuration;
-
-	// how long has the current image been displayed?
-	this.currentDisplayTime = 0;
-
-	// which image is currently being displayed?
-	this.currentTile = 0;
 
 	this.update = function( milliSec )
 	{
 		this.currentDisplayTime += milliSec;
+
 		while (this.currentDisplayTime > this.tileDisplayDuration)
 		{
 			this.currentDisplayTime -= this.tileDisplayDuration;
 			this.currentTile++;
+
 			if (this.currentTile == this.numberOfTiles)
 				this.currentTile = 0;
+
 			var currentColumn = this.currentTile % this.tilesHorizontal;
 			texture.offset.x = currentColumn / this.tilesHorizontal;
 			var currentRow = Math.floor( this.currentTile / this.tilesHorizontal );
