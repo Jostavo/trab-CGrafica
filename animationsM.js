c565625d9b1bca282dda2d8425a1e14040ab1a4a
João Vitor de Sá Medeiros Santos <jvsms13@gmail.com>
2016-07-20 15:50:09 -0300

    comentários >BEM< explicativos adicionados, agora nos eventos do megaman e animações

--- animationsM.js
+++ animationsM.js
@@ -1,177 +1,230 @@
-var TIROSPD = 5;
-
-var megaman = {x: 0, y: 0, z: 0, hp: 16};
-var megamanPlane;
+var TIROSPD = 5;//velocidade de movimento de projétil
+//-------------------OBJETOS GLOBAIS DE ANIMAÇÃO----------------------
+var megaman = {x: 0, y: 0, z: 0, hp: 16};//controle do personagem
+var megamanPlane;//geometria do personagem
+//objetos finais animados
 var standMegaman, walkMegaman, jumpMegaman, dashMegaman, pewpewMegaman;
+//objetos intermediários de textura
 var standMegamanTexture, walkMegamanTexture, jumpMegamanTexture, dashMegamanTexture, pewpewMegamanTexture, shotTexture, shotPoppingTexture;
+//objetos intermediários de material
 var standMegamanMaterial, walkMegamanMaterial, jumpMegamanMaterial, dashMegamanMaterial, pewpewMegamanMaterial, shotMaterial, shotPoppingMaterial;
+//objetos intermediários de animação de textura
 var standMegamanAnim, walkMegamanAnim, jumpMegamanAnim, dashMegamanAnim, pewpewMegamanAnim, shotAnim, shotPoppingAnim;
+//-------------------OBJETOS GLOBAIS DE ANIMAÇÃO----------------------
 
-var animEsquerda = false;
-var standingClock = 3, runningClock = 5;
+//-------------------OBJETOS GLOBAIS DE CONTROLE DE ANIMAÇÃO----------------------
+var animEsquerda = false;//flag para espelhamento de animação
+var standingClock = 3, runningClock = 5;//temporizadores de atualização p/ 'parado' e 'correndo'
+//-------------------OBJETOS GLOBAIS DE CONTROLE DE ANIMAÇÃO----------------------
 
-function initAnim(x, y, z)
+function initAnim(x, y, z)//FUNÇÃO DE CONTROLE DE ANIMAÇÃO
 {
-    // Esse código impede que o threeJS redimensione as imagens, bugando o animator
-		standMegamanTexture = new THREE.ImageUtils.loadTexture('sprites/mmx/standingmmx.png');
-    standMegamanTexture.minFilter = THREE.LinearFilter;
-    walkMegamanTexture = new THREE.ImageUtils.loadTexture('sprites/mmx/walkingmmx.png');
-    walkMegamanTexture.minFilter = THREE.LinearFilter;
-		//megaman atirando
-		pewpewMegamanTexture = new THREE.ImageUtils.loadTexture('sprites/mmx/shootingmmx.png');
-    pewpewMegamanTexture.minFilter = THREE.LinearFilter;
-		//tiro & tiro explodindo
-		shotTexture = new THREE.ImageUtils.loadTexture('sprites/mmx/shot1.png');
-		shotTexture.minFilter = THREE.LinearFilter;
-		shotPoppingTexture = THREE.ImageUtils.loadTexture('sprites/mmx/shot_pop.png');
-		shotPoppingTexture.minFilter = THREE.LinearFilter;
+		//------------------------CONTROLE DE TEXTURA-------------------------------
+		standMegamanTexture = new THREE.ImageUtils.loadTexture('sprites/mmx/standingmmx.png');//text. 'parado'
+    standMegamanTexture.minFilter = THREE.LinearFilter;//impede redimensionamento automático de imagem.
+    walkMegamanTexture = new THREE.ImageUtils.loadTexture('sprites/mmx/walkingmmx.png');//text. 'andando'
+    walkMegamanTexture.minFilter = THREE.LinearFilter;//impede redimensionamento automático de imagem.
+		pewpewMegamanTexture = new THREE.ImageUtils.loadTexture('sprites/mmx/shootingmmx.png');//text. 'atirando'
+    pewpewMegamanTexture.minFilter = THREE.LinearFilter;//impede redimensionamento automático de imagem.
+		shotTexture = new THREE.ImageUtils.loadTexture('sprites/mmx/shot1.png');//text. do projétil
+		shotTexture.minFilter = THREE.LinearFilter;//impede redimensionamento automático de imagem.
+		shotPoppingTexture = THREE.ImageUtils.loadTexture('sprites/mmx/shot_pop.png');//text. de colisão projétil
+		shotPoppingTexture.minFilter = THREE.LinearFilter;//impede redimensionamento automático de imagem.
+		//------------------------CONTROLE DE TEXTURA-------------------------------
 
-    // Aqui a gente cria os animators pra cada parte do programa
-    standMegamanAnim = new TextureAnimator(standMegamanTexture, 1, 3, 3, 30);
-    walkMegamanAnim = new TextureAnimator(walkMegamanTexture, 1, 11, 11, 30);
+		//-------------------CONTROLE DE ANIMAÇÃO DE TEXTURA-------------------------
+		/*o objeto contém as "fatias" de texturas sequenciadas numa animação
+		params= textura à fatiar, fatias na hor., fatias na ver., total de fatias,
+		temp de atualização*/
+		standMegamanAnim = new TextureAnimator(standMegamanTexture, 1, 3, 3, 30);//'parado'
+    walkMegamanAnim = new TextureAnimator(walkMegamanTexture, 1, 11, 11, 30);//'andando'
 		//novas animações
-		pewpewMegamanAnim = new TextureAnimator(pewpewMegamanTexture, 1, 1, 1, 30);
-		shotAnim = new TextureAnimator(shotTexture, 1, 1, 1, 30);
-		shotPoppingAnim = new TextureAnimator(shotPoppingTexture, 1, 3, 3, 30);
+		pewpewMegamanAnim = new TextureAnimator(pewpewMegamanTexture, 1, 1, 1, 30);//'atirando'
+		shotAnim = new TextureAnimator(shotTexture, 1, 1, 1, 30);//'projétil'
+		shotPoppingAnim = new TextureAnimator(shotPoppingTexture, 1, 3, 3, 30);//'colisão de projétil'
+		//-------------------CONTROLE DE ANIMAÇÃO DE TEXTURA------------------------
 
-    // Aqui cria-se os Meshes
-    standMegamanMaterial = new THREE.MeshBasicMaterial( { map: standMegamanTexture, side: THREE.DoubleSide, transparent: true} );
-    walkMegamanMaterial = new THREE.MeshBasicMaterial( { map: walkMegamanTexture, side: THREE.DoubleSide, transparent: true} );
-		//novos Meshes
-		pewpewMegamanMaterial = new THREE.MeshBasicMaterial( { map: pewpewMegamanTexture, side: THREE.DoubleSide, transparent: true} );
-		shotMaterial = new THREE.MeshBasicMaterial( { map: shotTexture, side: THREE.DoubleSide, transparent: true} );
-		shotPoppingMaterial = new THREE.MeshBasicMaterial( { map: shotPoppingMaterial, side: THREE.DoubleSide, transparent: true} );
+		//-------------------MATERIAL DE ANIMAÇÃO DE TEXTURA------------------------
+    /*Criação do material(como reage a luz, etc.) que será aplicado à geometria.
+		Nesse material,a textura é adicionada. No caso, as sprites carregadas
+		anteriormente*/
+		//VERIFICAR O MATERIAL USADO PARA REAGIR COM A LUZ!
+    standMegamanMaterial = new THREE.MeshBasicMaterial({ //megaman 'parado'
+			 map: standMegamanTexture, side: THREE.DoubleSide, transparent: true} );
+    walkMegamanMaterial = new THREE.MeshBasicMaterial({ //megaman 'andando'
+			map: walkMegamanTexture, side: THREE.DoubleSide, transparent: true} );
+		pewpewMegamanMaterial = new THREE.MeshBasicMaterial({ //megaman 'atirando'
+			map: pewpewMegamanTexture, side: THREE.DoubleSide, transparent: true} );
+		shotMaterial = new THREE.MeshBasicMaterial({ //projétil
+			map: shotTexture, side: THREE.DoubleSide, transparent: true} );
+		shotPoppingMaterial = new THREE.MeshBasicMaterial({ //colisão do projétil
+			map: shotPoppingTexture, side: THREE.DoubleSide, transparent: true} );
+		//-------------------MATERIAL DE ANIMAÇÃO DE TEXTURA------------------------
 
-
-    // E o plano
+    //-------------------GEOMETRIA DA ANIMAÇÃO---------------------------
     megamanPlane = new THREE.PlaneGeometry( 600,679 );
 		shotPlane = new THREE.PlaneGeometry(300,270);
+		//-------------------GEOMETRIA DA ANIMAÇÃO---------------------------
 
-    // Aqui mescla-se os planos com os materiais
-    standMegaman = new THREE.Mesh(megamanPlane, standMegamanMaterial);
+    //-------------------FUSÃO DE ANIMAÇÃO DE TEXTURA---------------------------
+		/*Combina-se a geometria desejada com a o material (como reage a luz e etc,
+		juntamente com a textura carregada) que deve ser aplicado*/
+		standMegaman = new THREE.Mesh(megamanPlane, standMegamanMaterial);
     walkMegaman = new THREE.Mesh(megamanPlane, walkMegamanMaterial);
 		pewpewMegaman = new THREE.Mesh(megamanPlane, pewpewMegamanMaterial);
+		//VERIFICAR se faltou mesh do tiro
+		//-------------------FUSÃO DE ANIMAÇÃO DE TEXTURA---------------------------
 
-
-    // Aqui inicializa-se as variáveis
+		//-------------------ESCALA DE TAMANHO DE ANIMAÇÃO--------------------------
     standMegaman.scale.set(0.035,0.035,0.035);
     walkMegaman.scale.set(0.035,0.035,0.035);
 		pewpewMegaman.scale.set(0.035,0.035,0.035);
 		//shotPopping.scale.set(0.035,0.035,0.035);
+		//-------------------ESCALA DE TAMANHO DE ANIMAÇÃO--------------------------
 
-		animationPic = standMegaman;
-    animation = standMegamanAnim;
-    updateClock = standingClock;
-    megaman.x = x;
+		//-------------------ATUALIZAÇÃO P/ OBJETOS INTERNOS------------------------
+		animationPic = standMegaman;//Animação completa (com geometria)
+    animation = standMegamanAnim;//Animação de textura
+    updateClock = standingClock;//Temporizador de atualização
+		//-------------------ATUALIZAÇÃO P/ OBJETOS INTERNOS------------------------
+
+		//-------------------CAPTURA PARÂMETROS --------------------------
+    megaman.x = x; //inicializa os atributos do obj megaman
     megaman.y = y;
     megaman.z = z;
+		//posiciona a nossa animação onde o megaman deve estar
     animationPic.position.set(megaman.x, megaman.y, megaman.z);
+		//-------------------CAPTURA PARÂMETROS --------------------------
 }
 
-// Função que recebe como parâmetro a nova animação
-function changeAnim(novaAnim, novaImg, clockzin)
-{
-    // Caso esteja para a esquerda, volta a animação ao normal antes de salvar
+function changeAnim(novaAnim, novaImg, clockzin){//FUNÇÃO de troca de animação
+		//params = textura animada, obj final de animação, temporizador de animação
+		//-------------------TRATAMENTO DE ESPELHAMENTO--------------------------
+		//Quando a anim. está espelhada,volta a anim. ao normal antes de salvar
     if(animEsquerda == true){
-      animationPic.scale.x *= -1;
+      animationPic.scale.x *= -1;//desvira
       animEsquerda = false;
     }
+		//-------------------TRATAMENTO DE ESPELHAMENTO--------------------------
 
-    // Salva as coordenadas
+		//-------------------MANTÉM COORDENADAS DE ANIMAÇÃO-------------------------
     megaman.x = animationPic.position.x;
     megaman.y = animationPic.position.y;
     megaman.z = animationPic.position.z;
-    // Remove da cena
-    scene.remove(animationPic);
+		//-------------------MANTÉM COORDENADAS DE ANIMAÇÃO-------------------------
 
-    // Muda a animação que irá aparecer
-    animation = novaAnim;
-    animationPic = novaImg;
-    updateClock = clockzin;
-
-    // Seta na nova animação a localização da antiga
-    animationPic.position.set(megaman.x,megaman.y,megaman.z);
-    // Adiciona na cena
-    scene.add(animationPic);
+		//-------------------CONTROLE DE CENA--------------------------
+		scene.remove(animationPic); //Remove da cena o objeto animado antigo
+			//-----------------ATUALIZAÇÃO P/ OBJETOS INTERNOS----------------------
+		animation = novaAnim; //nova textura animada
+    animationPic = novaImg; //novo objeto final animado
+    updateClock = clockzin; //novo temporizador
+		//VERIFICAR necessidade do temporizador nessa fç
+			//-----------------ATUALIZAÇÃO P/ OBJETOS INTERNOS----------------------
+		//Posiciona o novo objeto final na posição do objeto final antigo
+		animationPic.position.set(megaman.x,megaman.y,megaman.z);
+    scene.add(animationPic);// Adiciona na cena
+		//-------------------CONTROLE DE CENA--------------------------
 }
-
-//apenas usada para mostrar o megaman para o lado direito
-function changeAnim2(novaAnim, novaImg, clockzin)
-{
-    // Salva as coordenadas
+/*FUNÇÃO SIMILAR A FUNÇÃO ACIMA (changeAnim),
+apenas usada para mostrar o megaman para o lado direito,
+pois ela não não tem controle de espelhamento interno.
+COMENTÁRIOS mais explicativos são encontrados na fç changeAnim()*/
+function changeAnim2(novaAnim, novaImg, clockzin){
+    // Salva as coordenadas antigas do objeto final
     megaman.x = animationPic.position.x;
     megaman.y = animationPic.position.y;
     megaman.z = animationPic.position.z;
-    // Remove da cena
-    scene.remove(animationPic);
-
-    // Muda a animação que irá aparecer
+    scene.remove(animationPic);// Remove objeto final antigo da cena
+    // Muda os objetos de controle para texturas/animações novas novas
     animation = novaAnim;
     animationPic = novaImg;
     updateClock = clockzin;
-
-    // Seta na nova animação a localização da antiga
+    // Atribui a posição do objeto finalantigo ao objeto finalnovo
     animationPic.position.set(megaman.x,megaman.y,megaman.z);
-
-    // Adiciona na cena
-    scene.add(animationPic);
+    scene.add(animationPic); // Adiciona na cena
 }
 
-// Função que vira a animação
-function changeSide()
-{
+function changeSide(){ //FUNÇÃO de controle de espelhamento da animação
+	//------------------ESPELHAMENTO PARA DIREITA--------------------------
 	if(animEsquerda == true){
 		animEsquerda = false;
   	animationPic.scale.x *= -1;
+	//------------------ESPELHAMENTO PARA DIREITA--------------------------
+
+	//------------------ESPELHAMENTO PARA ESQUERDA--------------------------
 	}else{
 		animEsquerda = true;
   	animationPic.scale.x *= -1;
+	//------------------ESPELHAMENTO PARA ESQUERDA--------------------------
 	}
 }
 
-function shotSpawn()
-{
-	var shot = new THREE.Mesh(shotPlane, shotMaterial);
-	shot.scale.set(0.035,0.035,0.035);
-	if(animEsquerda == true){
+function shotSpawn(){ //FUNÇÃO para inserção de projéteis na cena
+	//------------------CONTROLE DE OBJETO DE PROJÉTIL--------------------------
+	var shot = new THREE.Mesh(shotPlane, shotMaterial);//fusão de geometria + material
+	shot.scale.set(0.035,0.035,0.035);//escala do tamanho do objeto final de animação
+	//------------------CONTROLE DE OBJETO DE PROJÉTIL--------------------------
+
+	//------------------CONTROLE DE POSIÇÃO DE PROJÉTIL--------------------------
+	/*Esta rotina leva em consideração que lado o megaman está apontando,
+	 para saber em que lado o tiro deverá aparecer.
+	 O projétil tem uma distância mínima do centro do objeto do Megaman*/
+	if(animEsquerda == true){//megaman apontado p/ esquerda
 			shot.position.set(megaman.x - 15, megaman.y+1.6, megaman.z);
 	}
-	else{
-			shot.position.set(megaman.x + 15, megaman.y+1.6, megaman.z)
+	else{//megaman apontado p/ direita
+			shot.position.set(megaman.x + 15, megaman.y+1.6, megaman.z);
 	}
-	//multiplos tiros
+	//------------------CONTROLE DE OBJETO DE PROJÉTIL--------------------------
+	//------------------CONTROLE DE OBJETO DE CENA--------------------------
+	//adiciona um projétil novo no objeto de controle de projéteis ativos na cena
 	shots.push(shot);
-	scene.add(shot);
+	scene.add(shot);//adiciona o projétil novo na cena
+	//------------------CONTROLE DE OBJETO DE CENA--------------------------
 }
 
-//itera de trás pra frente, pra poder remover tiro mais antigo
-function animaShots()
-{
-	for(var i = shots.length-1; i>=0; i--)
-	{
-		var shotTemp = shots[i];
-		var posX = shotTemp.position.x;
-
+function animaShots(){ //FUNÇÃO de animação de projétil	(movimentação)
+	/*Percorre-se o objeto de projéteis ativos de trás pra frente,
+	 para poder mover/remover tiro mais antigo na tela primeiro (o que foi 'atirado antes')*/
+	//laço decrementativo do tiro mais 'velho' ao mais 'novo'
+	for(var i = shots.length-1; i>=0; i--){
+		//------------------OBJETOS DE CONTROLE--------------------------
+		var shotTemp = shots[i];//objeto aponta para o projétil atual do passo
+		var posX = shotTemp.position.x; //objeto para guardar a posição atual do projétil
+		//------------------OBJETOS DE CONTROLE--------------------------
+		//------------------COLISÃO DE PROJÉTIL--------------------------
 		//colisão com a parece esquerda
-		if (posX < -3780){
-			//remove do vetor o projetil que colidiu
-			//inserir anim de shot pop
-			shots.splice(i,1);//posição, numero de elementos a remover
-			scene.remove(shotTemp);
+		if (posX < -3780){//colisão com a parece esquerda (está em x = -3780)
+			/*remove do vetor de objeto de projétil o qual colidiu com a parede
+			VERIFICAR colisão com outros objetos e ao sair da visão câmera*/
+			//'splice' remove um obj do índice desejado
+			shots.splice(i,1);//params.=posição, numero de elementos a remover
+			scene.remove(shotTemp);//retira o projétil da cena
 		}
-		else {//se nao colidiu, anima
-			if (posX < animationPic.position.x) {//tiro a esquerda
+		//------------------COLISÃO DE PROJÉTIL--------------------------
+		else {//O projétil que não colide permanece em movimento
+			//----------------CONTROLE DE DIREÇÃO DO MOVIMENTO------------------------
+			/*Quando a posição X do tiro é menor que a do objeto final do Megaman,
+			obrigatoriamente o projétil está à esquerda do Megaman. */
+			if (posX < animationPic.position.x){
+				//movimento de espaço 'TIROSPD' para a esquerda
 				shotTemp.translateX( -TIROSPD)
 			}
+			/*Inversamente, quando a posição X do tiro é MAIOR que a do objeto final
+			do Megaman, obrigatoriamente o projétil está à direta do Megaman. */
 			else {
+				//movimento de espaço 'TIROSPD' para a direita
 				shotTemp.translateX(TIROSPD)
 			}
+			//----------------CONTROLE DE DIREÇÃO DO MOVIMENTO------------------------
 		}
 
 	}
 }
 
-// Função de animação
+/*FUNÇÃO padrão de animação de textura
+Extraída de: https://stemkoski.github.io/Three.js/Texture-Animation.html*/
 function TextureAnimator(texture, tilesHoriz, tilesVert, numTiles, tileDispDuration)
 {
 	this.currentTile = 0;
