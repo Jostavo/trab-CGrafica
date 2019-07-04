9a41a6ff9d7a9fe69311434a0854a45a3216d0ff
Gustavo Rodrigues <grr.pride@gmail.com>
2019-07-04 08:58:45 -0300

    modified javascript library and started working on showing some of the sprites

--- index.js
+++
@@ -1,9 +0,0 @@
-var scene, camera, renderer, keyboard;
-
-var updateClock;
-
-var clock = new THREE.Clock();
-keyboard = new KeyboardState();
-
-init();
-animate();
---
+++ sketch.js
@@ -0,0 +1,19 @@
+var keyboard = new KeyboardState();
+var stage;
+var megaman;
+var props = [];
+
+function setup() {
+	createCanvas(600, 420);
+	stage = new Stage();
+}
+
+function draw() {
+	stage.show();
+}
+
+function keyPressed() {
+	// if (keyCode === LEFT_ARROW) {
+	// 	left();
+	// }
+}
